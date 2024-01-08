0ab0f3716ffa94ddb40226beced6dbeabd344275
FerMossier <mossierfernando@gmail.com>
2024-01-08 14:22:37 -0300

    Se crea funcionalidad de hola mundo

---
+++ GitColaborativo/Controllers/HolaMundoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitColaborativo.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class HolaMundoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetHolaMundo()
+        {
+            return Ok( new { data = "Hola Mundo" } );
+        }
+    }
+}
