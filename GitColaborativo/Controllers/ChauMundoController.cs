3c81be947f276239dcf2fb4c7f28f563a1e1a199
Fer Mossier <81035357+ferMossier@users.noreply.github.com>
2024-01-08 14:47:58 -0300

    Update ChauMundoController.cs
    Se agrega comentario de prueba

--- GitColaborativo/Controllers/ChauMundoController.cs
+++ GitColaborativo/Controllers/ChauMundoController.cs
@@ -9,6 +9,7 @@ namespace GitColaborativo.Controllers
         [HttpGet]
         public async Task<IActionResult> GetChauMundo( string name )
         {
+        //Comentario de prueba
             return Ok( new { data = $"Chau Mundo, soy {name} y me tengo que ir" } );
         }
 
